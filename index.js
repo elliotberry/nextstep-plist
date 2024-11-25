39de667622b2a5534da944206895e199a7f300ac
ɛʟʟɨօȶ b  =^._.^= ∫ <elliot@email.gd>
2024-11-24 21:22:26 -0500

    package cleaning

--- index.js
+++ index.js
@@ -1,56 +1,56 @@
 #!/usr/bin/env node
+import { EOL } from "node:os";
 import parse from "./lib/parse.js";
 import stringify from "./lib/stringify.js";
 
-
-
 async function getStdin() {
-	return new Promise((honour, betray) => {
-		let result = ""
+	return new Promise((resolve, reject) => {
+		let result = "";
+		process.stdin.setEncoding("utf8");
 
 		process.stdin.on("data", chunk => {
-			result += chunk
-		})
+			result += chunk;
+		});
 
-		process.stdin.on("end", () => honour(result))
-
-		process.stdin.on("error", betray)
+		process.stdin.on("end", () => resolve(result));
+		process.stdin.on("error", reject);
 	});
 }
 
-let help = `
+const help = `
 commands:
-	to-json	— take stdin as a plist and output JSON
-	to-plist	— take stdin as JSON and output plist
-`
+	to-json  — take stdin as a plist and output JSON
+	to-plist — take stdin as JSON and output plist
+`;
 
-;(async function monkey() {
+(async function main() {
 	if (!module.parent) {
-    let {EOL} = require("os")
-		switch (process.argv[2]) {
-			case "to-json": {
-				let input = await getStdin()
-				let object = parse(input)
-				let output = JSON.stringify(object, null, "\t")
-				return process.stdout.write(output + EOL)
+		try {
+			const command = process.argv[2];
+			const input = await getStdin();
+
+			switch (command) {
+				case "to-json": {
+					const object = parse(input);
+					const output = JSON.stringify(object, null, "\t");
+					process.stdout.write(output + EOL);
+					break;
+				}
+				case "to-plist": {
+					const object = JSON.parse(input);
+					const output = stringify(object);
+					process.stdout.write(output + EOL);
+					break;
+				}
+				default: {
+					process.stdout.write(help);
+					process.exitCode = 1;
+				}
 			}
-			case "to-plist": {
-				let input = await getStdin()
-				let object = JSON.parse(input)
-				let output = stringify(object)
-				return process.stdout.write(output + EOL)
-			}
-			default: {
-				process.stdout.write(help)
-				return process.exit(1)
-			}
+		} catch (error) {
+			console.error(process.env.debug ? error : error.message);
 		}
 	}
-})().catch(error => {
-  console.error(process.env.debug ? error : error.message)
-})
+})();
 
-export {
-	parse,
-	stringify,
-};
+export { parse, stringify };
